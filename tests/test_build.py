edf499a8a307a9f9dd11351c07eac6f34c49438a
zynthec <75141970+zynthec-dev@users.noreply.github.com>
2026-09-19 19:12:49 +0200

    Update liveMic to 1.0.5

--- tests/test_build.py
+++ tests/test_build.py
@@ -29,7 +29,7 @@ class BuildTests(unittest.TestCase):
         self.assertEqual(app["versions"][0]["marketingVersion"], "0.3-beta")
 
     @unittest.skipUnless(
-        (ROOT / "liveMic-1.0.3.ipa").exists() and (ROOT / "YouTube-Music-Ultimate-2.4.1_9.37.2-no-cast.ipa").exists(),
+        (ROOT / "liveMic-1.0.5.ipa").exists() and (ROOT / "YouTube-Music-Ultimate-2.4.1_9.37.2-no-cast.ipa").exists(),
         "liveMic and YouTube Music release assets are not available",
     )
     def test_filename_versions_and_names_are_separate(self):
@@ -38,7 +38,7 @@ class BuildTests(unittest.TestCase):
             app["name"]: app["versions"][0]["marketingVersion"]
             for app in source["apps"]
         }
-        self.assertEqual(versions["liveMic"], "1.0.3")
+        self.assertEqual(versions["liveMic"], "1.0.5")
         self.assertEqual(versions["YouTube Music Ultimate"], "2.4.1_9.37.2-no-cast")
 
     def test_feed_has_no_private_origin_metadata(self):
@@ -59,7 +59,7 @@ class BuildTests(unittest.TestCase):
         filenames = {app.get("ipaFile") for app in content["localApps"].values()}
         self.assertEqual(filenames, {
             "miPet-0.3-beta.ipa",
-            "liveMic-1.0.3.ipa",
+            "liveMic-1.0.5.ipa",
             "YouTube-Music-Ultimate-2.4.1_9.37.2-no-cast.ipa",
         })
 
